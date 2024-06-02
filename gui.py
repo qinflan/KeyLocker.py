93077afb5701835342e0ab521e250705fa0d3330
qinflan <158785365+qinflan@users.noreply.github.com>
2024-06-02 04:53:15 -0400

    UI Improvements
    Adjusted Alignment of frames.

--- gui.py
+++ gui.py
@@ -11,33 +11,42 @@ root = customtkinter.CTk()
 root.title("KeyLocker")
 root.iconbitmap("keylocker-dir/logo.ico")
 root.geometry("1000x600")
+root.minsize(700,500)
+root.maxsize(1200,800)
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 # font objects
-my_font = customtkinter.CTkFont("DM Sans 14pt", size=24, weight="bold")
+output_font = customtkinter.CTkFont("DM Sans 14pt", size=24, weight="bold")
 btn = customtkinter.CTkFont("DM Sans 14pt", size=14, weight="bold")
 
-# label - insert image and logo here
+# logo header
 logo_img = customtkinter.CTkImage(dark_image=Image.open("keylocker-dir/logo+text.png"), 
-light_image=Image.open("keylocker-dir/logo+text.png"), size=(400, 80))
-
+light_image=Image.open("keylocker-dir/logo+text-black.png"), size=(300, 58))
 logo_label = customtkinter.CTkLabel(master=root, text="", image=logo_img)
-logo_label.place(rely=0.02, relx=0.05)
+logo_label.place(rely=0.02, relx=0.1)
+
 
 # frame containers
+#-----------------------
+
+# get/add password buttons
 frame1 = customtkinter.CTkFrame(master=root)
-frame1.pack(side="left", pady=100, padx=40, fill="both", expand=True)
+frame1.place(relx = 0.08, rely = 0.15, relwidth = 0.35, relheight = 0.38)
+
+# submission and output frame
 frame2 = customtkinter.CTkFrame(master=root)
-frame2.pack(side="right", pady=175, padx=40, fill="both", expand=True)
+frame2.place(relx = 0.47, rely = 0.15, relwidth = 0.45, relheight = 0.38)
+
+# grid frame for file/load buttons
+frame3 = customtkinter.CTkFrame(master=root)
+frame3.place(relx = 0.08, rely = 0.58, relheight = 0.38, relwidth = 0.84)
+frame3.columnconfigure((0,1,2), weight = 1)
+frame3.rowconfigure((0,1,2), weight = 1)
 
 # entry handling
-path_entry = customtkinter.CTkEntry(frame2, placeholder_text="Name key file", font=btn)
-path_entry.pack(pady=20)
-
-# output label display
-output_label = customtkinter.CTkLabel(frame2, text="", font=my_font)
-output_label.pack(pady=40)
+path_entry = customtkinter.CTkEntry(frame2, width=250, placeholder_text="Enter site for password", font=btn)
+path_entry.pack(pady=40)
 
 # Button Callback Functions
 def create_key_btn():
@@ -66,31 +75,102 @@ def get_password_btn():
     site = input("What site do you want: ")
     print(f"Password for {site} is {pm.get_password(site)}")
 
+# insert logic for pulling all keys from (key, value) pair in API
+def view_sites():
+    pass
+
 # Submission Handling
 def submit_path():
     user_input = path.get()
     return user_input
 
+def toggle_theme():
+    val=switch.get()
+    if val:
+        customtkinter.set_appearance_mode("light")
+    else:
+        customtkinter.set_appearance_mode("dark")
+
 # buttons
-button1 = customtkinter.CTkButton(master=frame1, width=210, height=50, text="Get a password", font=btn, command=get_password_btn)
-button1.pack(pady=14, padx=10)
+button1 = customtkinter.CTkButton(master=frame1, 
+    width=210, 
+    height=50, 
+    corner_radius=14, 
+    text="Retrieve password", 
+    font=btn, 
+    command=get_password_btn)
+button1.pack(pady=10, padx=0)
 
-button2 = customtkinter.CTkButton(master=frame1, width=210, height=50, text="Add a password", font=btn, command=add_password_btn)
-button2.pack(pady=14, padx=10)
+button2 = customtkinter.CTkButton(master=frame1, 
+    width=210, 
+    height=50,
+    corner_radius=14,
+    text="Add a password", 
+    font=btn, 
+    command=add_password_btn)
+button2.pack(pady=10, padx=10)
 
-button3 = customtkinter.CTkButton(master=frame1, width=210, height=50, text="Create a new password file", font=btn, command=create_password_file_btn)
-button3.pack(pady=14, padx=10)
+button3 = customtkinter.CTkButton(master=frame1, 
+    width=210, 
+    height=50,
+    corner_radius=14,
+    text="View used sites", 
+    font=btn, 
+    command=add_password_btn)
+button3.pack(pady=10, padx=10)
 
-button4 = customtkinter.CTkButton(master=frame1, width=210, height=50, text="Load existing password file", font=btn, command=load_password_file_btn)
-button4.pack(pady=14, padx=10)
+button4 = customtkinter.CTkButton(master=frame3, 
+    width=250, 
+    height=50,
+    corner_radius=14,
+    text="Create new password file", 
+    font=btn, 
+    command=create_password_file_btn)
+button4.grid(row = 0, column = 0, sticky="se")
 
-button5 = customtkinter.CTkButton(master=frame1, width=210, height=50, text="Create a new key", font=btn, command=create_key_btn)
-button5.pack(pady=14, padx=10)
+button5 = customtkinter.CTkButton(master=frame3, 
+    width=250, 
+    height=50,
+    corner_radius=14,
+    text="Load password file", 
+    font=btn, 
+    command=load_password_file_btn)
+button5.grid(row = 0, column = 2, sticky="sw")
 
-button6 = customtkinter.CTkButton(master=frame1, width=210, height=50, text="Load an existing key", font=btn, command=load_key_btn)
-button6.pack(pady=14, padx=10)
+button6 = customtkinter.CTkButton(master=frame3, 
+    width=250, 
+    height=50,
+    corner_radius=14,
+    text="Create a new key", 
+    font=btn, 
+    command=create_key_btn)
+button6.grid(row = 2, column = 0, sticky="ne")
 
-submit_button = customtkinter.CTkButton(master=frame2, width=100, height=50, text="Submit", font=btn, command=submit_path)
-submit_button.pack(pady=20)
+button7 = customtkinter.CTkButton(master=frame3, 
+    width=250, 
+    height=50,
+    corner_radius=14,
+    text="Load existing key",
+    font=btn, 
+    command=load_key_btn)
+button7.grid(row = 2, column = 2, sticky="nw")
+
+submit_button = customtkinter.CTkButton(master=frame2,
+    width=100, 
+    height=40,
+    corner_radius=14,
+    text="Submit", 
+    font=btn, 
+    command=submit_path)
+submit_button.pack(side="top")
+
+# light mode/dark mode switch
+switch = customtkinter.CTkSwitch(root, text='Toggle Theme',
+    font=btn,
+    onvalue=1,
+    offvalue=0,
+    command=toggle_theme)
+switch.place(relx = 0.72, rely = 0.06)
+print(switch.get())
 
 root.mainloop()
