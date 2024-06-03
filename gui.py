919a3ffb69da2a32004e89fdb25d04cc90effb8d
qinflan <158785365+qinflan@users.noreply.github.com>
2024-06-02 20:04:53 -0400

    Fix key creation/loading buttons
    Fixes issues with calling key related API functions from buttons, added dynamic features to UI.

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ root = customtkinter.CTk()
 root.title("KeyLocker")
 root.iconbitmap("keylocker-dir/logo.ico")
 root.geometry("1000x600")
-root.minsize(700,500)
+root.minsize(700,580)
 root.maxsize(1200,800)
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
@@ -37,6 +37,8 @@ frame1.place(relx = 0.08, rely = 0.15, relwidth = 0.35, relheight = 0.38)
 # submission and output frame
 frame2 = customtkinter.CTkFrame(master=root)
 frame2.place(relx = 0.47, rely = 0.15, relwidth = 0.45, relheight = 0.38)
+frame2.columnconfigure((0), weight = 1)
+frame2.rowconfigure((0,1,2,3), weight = 1)
 
 # grid frame for file/load buttons
 frame3 = customtkinter.CTkFrame(master=root)
@@ -45,44 +47,61 @@ frame3.columnconfigure((0,1,2), weight = 1)
 frame3.rowconfigure((0,1,2), weight = 1)
 
 # entry handling
-path_entry = customtkinter.CTkEntry(frame2, width=250, placeholder_text="Enter site for password", font=btn)
-path_entry.pack(pady=40)
+entry_box = customtkinter.CTkEntry(frame2, width=250, placeholder_text="Select an option", font=btn)
+entry_box.grid(row = 0)
+password_entry = customtkinter.CTkEntry(frame2, width=250, placeholder_text="Enter password", font=btn, show="●")
+
+
+# Initialize function variable to dynamically change, as well as output msg
+current_function = None
+output_msg = ""
+
+# Submission Handling
+def submit_input():
+    user_input = entry_box.get()
+    print(user_input)
+    if current_function:
+        current_function(user_input)
+    output_label.configure(text=output_msg)
 
 # Button Callback Functions
 def create_key_btn():
-    path = path_entry.get()
-    pm.create_key(path)
-    output_label.configure(text=f"Created key at: {path}")
+    entry_box.configure(placeholder_text="Specify name for key")
+    global current_function, output_msg
+    current_function = pm.create_key
+    output_msg = "Key Successfully Created"
 
 def load_key_btn():
-    path = input("Enter path: ")
-    pm.load_key(path)
+    entry_box.configure(placeholder_text="Specify key filename")
+    global current_function
+    current_function = pm.load_key
 
 def create_password_file_btn():
-    path = input("Enter path: ")
-    pm.create_password_file(path, password)
+    entry_box.configure(placeholder_text="Specify name for password file")
+    global current_function
+    current_function = pm.create_password_file
 
 def load_password_file_btn():
+    entry_box.configure(placeholder_text="Specify password filename")
     path = input("Enter path: ")
     pm.load_password_file(path)
 
 def add_password_btn():
+    entry_box.configure(placeholder_text="Enter site/service name")
+    password_entry.grid(row = 1)
+
     site = input("Enter the site: ")
     password = input("Enter the password: ")
     pm.add_password(site, password)
             
 def get_password_btn():
+    entry_box.configure(placeholder_text="Enter site for password")
     site = input("What site do you want: ")
     print(f"Password for {site} is {pm.get_password(site)}")
 
 # insert logic for pulling all keys from (key, value) pair in API
 def view_sites():
     pass
-
-# Submission Handling
-def submit_path():
-    user_input = path.get()
-    return user_input
 
 def toggle_theme():
     val=switch.get()
@@ -155,14 +174,15 @@ button7 = customtkinter.CTkButton(master=frame3,
     command=load_key_btn)
 button7.grid(row = 2, column = 2, sticky="nw")
 
+# submission button
 submit_button = customtkinter.CTkButton(master=frame2,
     width=100, 
     height=40,
     corner_radius=14,
     text="Submit", 
     font=btn, 
-    command=submit_path)
-submit_button.pack(side="top")
+    command=submit_input)
+submit_button.grid(row = 2)
 
 # light mode/dark mode switch
 switch = customtkinter.CTkSwitch(root, text='Toggle Theme',
@@ -172,5 +192,9 @@ switch = customtkinter.CTkSwitch(root, text='Toggle Theme',
     command=toggle_theme)
 switch.place(relx = 0.72, rely = 0.06)
 print(switch.get())
+
+#output label
+output_label = customtkinter.CTkLabel(master = frame2, text="", font = btn)
+output_label.grid(row = 3)
 
 root.mainloop()
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class PasswordManager:
 
     def create_password_file(self, path, initial_values=None):
         self.password_file = path
-        
+    
         if initial_values is not None:
             for key, value in initial_values.items():
                 self.add_password(key, value)
@@ -45,6 +45,7 @@ class PasswordManager:
 
 
 def main():
+
     #test case
     password = {
         "email": "1234567",
@@ -55,53 +56,53 @@ def main():
 
     pm = PasswordManager()
 
-#Command Line Interface
-    # print("""What do you want to do?
-    #     (1) Create a new key
-    #     (2) Load an existing key
-    #     (3) Create a new password file
-    #     (4) Load existing password file
-    #     (5) Add a new password
-    #     (6) Get a password
-    #     (q) Quit
-    #     """)
+# Command Line Interface
+    print("""What do you want to do?
+        (1) Create a new key
+        (2) Load an existing key
+        (3) Create a new password file
+        (4) Load existing password file
+        (5) Add a new password
+        (6) Get a password
+        (q) Quit
+        """)
 
-    # done = False
+    done = False
 
-    # while not done:
-    #     choice = input("Enter your choice: ")
+    while not done:
+        choice = input("Enter your choice: ")
 
-    #     if choice == "1":
-    #         path = input("Enter path: ")
-    #         pm.create_key(path)
+        if choice == "1":
+            path = input("Enter path: ")
+            pm.create_key(path)
 
-    #     elif choice == "2":
-    #         path = input("Enter path: ")
-    #         pm.load_key(path)
+        elif choice == "2":
+            path = input("Enter path: ")
+            pm.load_key(path)
 
-    #     elif choice == "3":
-    #         path = input("Enter path: ")
-    #         pm.create_password_file(path, password)
+        elif choice == "3":
+            path = input("Enter path: ")
+            pm.create_password_file(path)
 
-    #     elif choice == "4":
-    #         path = input("Enter path: ")
-    #         pm.load_password_file(path)
+        elif choice == "4":
+            path = input("Enter path: ")
+            pm.load_password_file(path)
 
-    #     elif choice == "5":
-    #         site = input("Enter the site: ")
-    #         password = input("Enter the password: ")
-    #         pm.add_password(site, password)
+        elif choice == "5":
+            site = input("Enter the site: ")
+            password = input("Enter the password: ")
+            pm.add_password(site, password)
             
-    #     elif choice == "6":
-    #         site = input("What site do you want: ")
-    #         print(f"Password for {site} is {pm.get_password(site)}")
+        elif choice == "6":
+            site = input("What site do you want: ")
+            print(f"Password for {site} is {pm.get_password(site)}")
             
-    #     elif choice == "q":
-    #         done = True
-    #         print("Enjoy!")
+        elif choice == "q":
+            done = True
+            print("Enjoy!")
 
-    #     else:
-    #         print("Invalid Choice")
+        else:
+            print("Invalid Choice")
 
 if __name__ == "__main__":
     main()
